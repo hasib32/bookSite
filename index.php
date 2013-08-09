b79e54851fe1d0aa0e3135d0c6210aba56c51dfc
Hasan Hasibul <hasib32@gmail.com>
2013-08-09 13:29:35 -0400

    added select option depeatment

--- index.php
+++ index.php
@@ -35,15 +35,65 @@
         <div id="select">
             <div class="col-lg-4">
                 <select>
-                    <option selected="">Select Department</option>
-                    <option>Acoounting</option>
-                    <option>Acoounting</option>
-                    <option>Acoounting</option>
-                    <option>Acoounting</option>
-                    <option>Acoounting</option>
-                    <option>Acoounting</option>
-                    <option>Acoounting</option>
-                    <option>Acoounting</option>
+                    <option selected="" value="All">All departments</option>
+                    <option value="Accounting">Accounting</option>
+                    <option value="Anthropology">Anthropology</option>
+                    <option value="Art History">Art History</option>
+                    <option value="Biological Sciences">Biological Sciences</option>
+                    <option value="Biology">Biology</option>
+                    <option value="Business">Business</option>
+                    <option value="Business Technology">Business Technology</option>
+                    <option value="Chemistry">Chemistry</option>
+                    <option value="Communication">Communication</option>
+                    <option value="Computer Science">Computer Science</option>
+                    <option value="Cooperative Education">Cooperative Education</option>
+                    <option value="Counseling">Counseling</option>
+                    <option value="Criminal Justice">Criminal Justice</option>
+                    <option value="Design">Design</option>
+                    <option value="Economics">Economics</option>
+                    <option value="Education">Education</option>
+                    <option value="Education Studies">Education Studies</option>
+                    <option value="EMT & Paramedic">EMT & Paramedic</option>
+                    <option value="Engineering">Engineering</option>
+                    <option value="English">English</option>
+                    <option value="English As A Second Language">English As A Second Language</option>
+                    <option value="Ethnic Studies">Ethnic Studies</option>
+                    <option value="Film">Film</option>
+                    <option value="Fine Arts">Fine Arts</option>
+                    <option value="Geography">Geography</option>
+                    <option value="Graphic Arts">Graphic Arts</option>
+                    <option value="Health Science">Health Science</option>
+                    <option value="History">History</option>
+                    <option value="Hospitality">Hospitality</option>
+                    <option value="Humanities">Humanities</option>
+                    <option value="Information Science">Information Science</option>
+                    <option value="International Studies">International Studies</option>
+                    <option value="Journalism">Journalism</option>
+                    <option value="Languages">Languages</option>
+                    <option value="Law">Law</option>
+                    <option value="Literature">Literature</option>
+                    <option value="Management">Management</option>
+                    <option value="Marketing">Marketing</option>
+                    <option value="Mathematics">Mathematics</option>
+                    <option value="Medicine">Medicine</option>
+                    <option value="Music">Music</option>
+                    <option value="Natural Sciences">Natural Sciences</option>
+                    <option value="Not Specified">Not Specified</option>
+                    <option value="Nursing">Nursing</option>
+                    <option value="Nutrition">Nutrition</option>
+                    <option value="Philosophy">Philosophy</option>
+                    <option value="Physical Ed">Physical Ed</option>
+                    <option value="Physical Education">Physical Education</option>
+                    <option value="Physics">Physics</option>
+                    <option value="Political Science">Political Science</option>
+                    <option value="Psychology">Psychology</option>
+                    <option value="Science">Science</option>
+                    <option value="Social Science">Social Science</option>
+                    <option value="Social Sciences">Social Sciences</option>
+                    <option value="Social Work">Social Work</option>
+                    <option value="Sociology">Sociology</option>
+                    <option value="Theater">Theater</option>
+                    <option value="Writing">Writing</option>
                 </select>
             </div>
         </div>
