0baf21b4d66ee1600dc992dfe21ec0cc2da1330f
Hasan Hasibul <hasib32@gmail.com>
2013-08-09 13:20:49 -0400

    change basic layout

--- index.php
+++ index.php
@@ -13,77 +13,74 @@
         <h2> Name and Logo goes Here</h2>
     </div>
     <div class="row">
-        <div class="col-lg-4">
-            <ul class="nav nav-pills">
-                <li><a href="#">Home</a> </li>
-                <li><a href="#">About Us</a> </li>
-            </ul>
+            <div id="left-nav">
+                <div class="col-lg-3">
+                    <ul class="nav nav-pills nav-stacked">
+                        <li ><a href="#">Home </a> </li>
+                        <li><a href="#">Buy Books</a> </li>
+                        <li><a href="#">Sell Books</a> </li>
+                        <li><a href="#">Books on Demand</a> </li>
+                        <li><a href="#">Request a Book</a> </li>
+                    </ul>
+                </div>
         </div>
-        <div class="col-lg-4">
-            <select>
-                <option >Select Department</option>
-                <option>Acoounting</option>
-                <option>Acoounting</option>
-                <option>Acoounting</option>
-                <option>Acoounting</option>
-                <option>Acoounting</option>
-                <option>Acoounting</option>
-                <option>Acoounting</option>
-                <option>Acoounting</option>
-            </select>
-        </div>
+
         <div id="search">
             <div class="col-lg-4">
                 <form class="navbar-search pull-left">
-                    <input type="text" class="search-query" placeholder="Search">
+                    <input type="text" class="search-query" placeholder="Search Here..">
                 </form>
+            </div>
+        </div>
+        <div id="select">
+            <div class="col-lg-4">
+                <select>
+                    <option selected="">Select Department</option>
+                    <option>Acoounting</option>
+                    <option>Acoounting</option>
+                    <option>Acoounting</option>
+                    <option>Acoounting</option>
+                    <option>Acoounting</option>
+                    <option>Acoounting</option>
+                    <option>Acoounting</option>
+                    <option>Acoounting</option>
+                </select>
+            </div>
+        </div>
+        <div id="content" >
+            <div class="col-lg-7 ">
+                <h3>Introduction to computer</h3>
+                <h4>By David Mayer</h4>
+                <p>This is book is look likes brand new</p>
+                <p>Seller: MR Mario</p>
+                <hr>
+                <h3>Introduction to computer</h3>
+                <h4>By David Mayer</h4>
+                <p>This is book is look likes brand new</p>
+                <p>Seller: MR Mario</p>
+                <hr>
+                <h3>Introduction to computer</h3>
+                <h4>By David Mayer</h4>
+                <p>This is book is look likes brand new</p>
+                <p>Seller: MR Mario</p>
+                <hr>
+                <h3>Introduction to computer</h3>
+                <h4>By David Mayer</h4>
+                <p>This is book is look likes brand new</p>
+                <p>Seller: MR Mario</p>
+                <hr><h3>Introduction to computer</h3>
+                <h4>By David Mayer</h4>
+                <p>This is book is look likes brand new</p>
+                <p>Seller: MR Mario</p>
+                <hr><h3>Introduction to computer</h3>
+                <h4>By David Mayer</h4>
+                <p>This is book is look likes brand new</p>
+                <p>Seller: MR Mario</p>
+                <hr>
             </div>
         </div>
     </div>
 
-        <div class="row">
-            <div id="left-nav">
-                <div class="col-lg-3">
-                    <ul class="nav nav-pills nav-stacked">
-                        <li ><a href="#">Accounting</a> </li>
-                        <li><a href="#">Business</a> </li>
-                        <li><a href="#">Psychology</a> </li>
-                        <li><a href="#">Language</a> </li>
-                        <li><a href="#">Math</a> </li>
-                    </ul>
-                </div>
-
-            </div>
-            <div class="col-lg-7">
-                <h3>Introduction to computer</h3>
-                <h4>By David Mayer</h4>
-                <p>This is book is look likes brand new</p>
-                <p>Seller: MR Mario</p>
-                <hr>
-                <h3>Introduction to computer</h3>
-                <h4>By David Mayer</h4>
-                <p>This is book is look likes brand new</p>
-                <p>Seller: MR Mario</p>
-                <hr>
-                <h3>Introduction to computer</h3>
-                <h4>By David Mayer</h4>
-                <p>This is book is look likes brand new</p>
-                <p>Seller: MR Mario</p>
-                <hr>
-                <h3>Introduction to computer</h3>
-                <h4>By David Mayer</h4>
-                <p>This is book is look likes brand new</p>
-                <p>Seller: MR Mario</p>
-                <hr><h3>Introduction to computer</h3>
-                <h4>By David Mayer</h4>
-                <p>This is book is look likes brand new</p>
-                <p>Seller: MR Mario</p>
-                <hr><h3>Introduction to computer</h3>
-                <h4>By David Mayer</h4>
-                <p>This is book is look likes brand new</p>
-                <p>Seller: MR Mario</p>
-                <hr>
-            </div>
         </div>
     <div class="panel-footer">Develop by mgbites.com </div>
 </div>
