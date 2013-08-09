89cb9b204525553b2cb7a3af84e110dce06b13f0
Hasan Hasibul <hasib32@gmail.com>
2013-08-09 10:42:41 -0400

    Initiliazition the project

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <title>Buy Text Books</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<div class="container">
+    <div class="page-header">
+        <h2> Name and Logo goes Here</h2>
+    </div>
+    <div class="row">
+        <div class="col-lg-4">
+            <ul class="nav nav-pills">
+                <li><a href="#">Home</a> </li>
+                <li><a href="#">About Us</a> </li>
+            </ul>
+        </div>
+        <div class="col-lg-4">
+            <select>
+                <option >Select Department</option>
+                <option>Acoounting</option>
+                <option>Acoounting</option>
+                <option>Acoounting</option>
+                <option>Acoounting</option>
+                <option>Acoounting</option>
+                <option>Acoounting</option>
+                <option>Acoounting</option>
+                <option>Acoounting</option>
+            </select>
+        </div>
+        <div id="search">
+            <div class="col-lg-4">
+                <form class="navbar-search pull-left">
+                    <input type="text" class="search-query" placeholder="Search">
+                </form>
+            </div>
+        </div>
+    </div>
+
+        <div class="row">
+            <div id="left-nav">
+                <div class="col-lg-3">
+                    <ul class="nav nav-pills nav-stacked">
+                        <li ><a href="#">Accounting</a> </li>
+                        <li><a href="#">Business</a> </li>
+                        <li><a href="#">Psychology</a> </li>
+                        <li><a href="#">Language</a> </li>
+                        <li><a href="#">Math</a> </li>
+                    </ul>
+                </div>
+
+            </div>
+            <div class="col-lg-7">
+                <h3>Introduction to computer</h3>
+                <h4>By David Mayer</h4>
+                <p>This is book is look likes brand new</p>
+                <p>Seller: MR Mario</p>
+                <hr>
+                <h3>Introduction to computer</h3>
+                <h4>By David Mayer</h4>
+                <p>This is book is look likes brand new</p>
+                <p>Seller: MR Mario</p>
+                <hr>
+                <h3>Introduction to computer</h3>
+                <h4>By David Mayer</h4>
+                <p>This is book is look likes brand new</p>
+                <p>Seller: MR Mario</p>
+                <hr>
+                <h3>Introduction to computer</h3>
+                <h4>By David Mayer</h4>
+                <p>This is book is look likes brand new</p>
+                <p>Seller: MR Mario</p>
+                <hr><h3>Introduction to computer</h3>
+                <h4>By David Mayer</h4>
+                <p>This is book is look likes brand new</p>
+                <p>Seller: MR Mario</p>
+                <hr><h3>Introduction to computer</h3>
+                <h4>By David Mayer</h4>
+                <p>This is book is look likes brand new</p>
+                <p>Seller: MR Mario</p>
+                <hr>
+            </div>
+        </div>
+    <div class="panel-footer">Develop by mgbites.com </div>
+</div>
+
+</body>
+</html>
